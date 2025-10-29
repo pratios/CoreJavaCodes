cc1e35815951726963600da035b734d6b07b4892
pratios <69676673+pratios@users.noreply.github.com>
2025-10-29 16:42:33 +0530

    Add files via upload

--- ArrayListExample.java
+++ ArrayListExample.java
@@ -1,42 +1,23 @@
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
 
 public class ArrayListExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-//		List<Integer> list = new ArrayList();
-		List<Integer> list = new LinkedList();
+		ArrayList<String> ar1 = new ArrayList<String>();
+		ar1.add("Hello");
+		ar1.add("there!");
+		System.out.println(ar1);
 		
-		list.add(10);
-		list.add(22);
-		list.add(33);
-		list.add(21);
-		list.add(56);
-		list.add(34);
-		list.add(32);
+		ArrayList<String> ar2 = new ArrayList<String>();
+		ar2.add("Second");
+		ar2.add("ArrayList");
+		System.out.println(ar2);
 		
-		System.out.println(list);
-		System.out.println("------------------------------------------------------------------");
+		ar1.addAll(ar2);
 		
-		for(int i : list) {
-			System.out.println(i);
-		}
-		
-		list.add(2,36);
-		System.out.println(list);
-		System.out.println("------------------------------------------------------------------");
-		list.remove(4);
-		System.out.println("Total : "+list.size());
-		System.out.println("------------------------------------------------------------------");
-		System.out.println(list.get(5));
-		System.out.println(list.contains(20));
-		System.out.println("------------------------------------------------------------------");
-		
-		for(Integer i : list)
-			System.out.println(i);
+		System.out.println(ar1);
 	}
 
 }
